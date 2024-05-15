e55065b97ba69d857a90d06fe48a6f50b8f50ef3
anaaragon88 <ana.aragon88@gmail.com>
2024-05-15 20:58:06 +0200

    start

---
+++ src/components/Experience.jsx
@@ -0,0 +1,12 @@
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+
+export const Experience = () => {
+  return (
+    <>
+      <OrbitControls />
+      <Avatar position={[0, -3, 5]} scale={2}/>
+      <Environment preset="sunset" /> 
+    </>
+  );
+};
